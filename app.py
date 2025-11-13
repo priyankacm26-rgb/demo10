e9346ec03c1961a1908619491343786fa7e041a0
Priya <Priyankacm26@gmail.com>
2025-11-13 09:51:58 +0530

    changed variable a to num

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-a = int(input("enter a number"))
+num = int(input("enter a number"))
 b = int(input("enter a  number"))
-if a % 2 == 0 & b % 2 == 0:
+if num % 2 == 0 & b % 2 == 0:
    print("Both Numbers are even ")
-elif a % 2 != 0 & b % 2 !=0:
+elif num % 2 != 0 & b % 2 !=0:
    print("Both number are odd ")
-elif a % 2 == 0 and b % 2 != 0:
+elif num % 2 == 0 and b % 2 != 0:
     print("First number is even and second number is odd.")
 else:
     print("First number is odd and second number is even.")
